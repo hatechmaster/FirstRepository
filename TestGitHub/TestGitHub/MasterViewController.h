b6e71e437637c1f516a3c9429a1aa7019d3435b6
Techmaster <Techmaster@Mac-08.local>
2012-08-01 15:22:53 +0700

    First init

---
+++ TestGitHub/TestGitHub/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  TestGitHub
+//
+//  Created by Techmaster on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
